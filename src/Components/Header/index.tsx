e0a8992574ce174027df817501031bddf2ee80df
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-08-19 21:09:16 -0300

    First Commit

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logo from "../../assets/icon/IconRatings.svg";
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <header>
+        <img src={logo} alt="Logo Ratings" />
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Pages/GodOfWar/index.tsx
@@ -0,0 +1,11 @@
+const GodOfWar: React.FC = () => {
+  return (
+    <>
+      {/*Header*/}
+      {/*Content*/}
+      {/**/}
+    </>
+  );
+};
+
+export default GodOfWar;
