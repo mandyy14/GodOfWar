b8fa4434e3982f5238ae48dfe3341eca1b58a877
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-08-22 16:47:44 -0300

    Created Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,34 @@
 import logo from "../../assets/icon/IconRatings.svg";
+import Menu from "../../assets/icon/IconMenu.svg";
+import Search from "../../assets/icon/IconSearch.svg";
+
 import "./styles.css";
+import Button from "../Button";
+
+const handleClick = () => {
+  alert("Oi prof Caio, você fez login no Ratings de mentirinha");
+};
 
 const Header: React.FC = () => {
   return (
     <>
       <header>
-        <img src={logo} alt="Logo Ratings" />
+        <div className="header-container">
+          <img src={logo} alt="Logo Ratings" className="header-logo" />
+          <div className="menu-container">
+            <img src={Menu} alt="Menu" />
+            <div className="menu-text">Menu</div>
+            <div className="search-field">
+              <img
+                src={Search}
+                alt="Lupa de pesquisa"
+                className="search-logo"
+              />
+              <div className="search-text">Search</div>
+            </div>
+            <Button onClick={handleClick} />
+          </div>
+        </div>
       </header>
     </>
   );
