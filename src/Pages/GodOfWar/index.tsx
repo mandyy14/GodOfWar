34c31519d18e8b07951a095289cac95dd3980912
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-08-22 16:51:08 -0300

    Updated Principal Page

--- src/Pages/GodOfWar/index.tsx
+++ src/Pages/GodOfWar/index.tsx
@@ -1,11 +1,13 @@
+import Footer from "../../Components/Footer";
 import Header from "../../Components/Header";
 
 const GodOfWar: React.FC = () => {
   return (
     <>
       <Header />
+      <br></br>
       {/*Content*/}
-      {/**/}
+      <Footer />
     </>
   );
 };
