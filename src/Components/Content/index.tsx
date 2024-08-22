f9a62495d586a91fb3a2df2b4deedc67a54ae44b
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-08-22 16:56:37 -0300

    Created Content

---
+++ src/Components/Content/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Content: React.FC = () => {
+  return (
+    <>
+      <div className="container">
+        <img></img>
+      </div>
+    </>
+  );
+};
+
+export default Content;
