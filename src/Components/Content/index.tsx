d6fa0a79c9c22c39ef96ac24116c16e308dcb231
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-08-24 19:35:51 -0300

    Updated

--- src/Components/Content/index.tsx
+++ src/Components/Content/index.tsx
@@ -1,4 +1,6 @@
 import Arrow from "../../assets/icon/IconArrow.svg";
+import Plus from "../../assets/icon/plus.svg";
+import Star from "../../assets/icon/star.svg";
 
 import "./styles.css";
 
@@ -21,8 +23,47 @@ const Content: React.FC = () => {
         <div className="align-plot">
           <img src={Arrow} alt="Seta" className="img-plot" />
           <div className="plot">AVALIAÇÕES DE USUÁRIOS</div>
+          <div className="align-A">
+            <img src={Plus} />
+            <div>Avaliar</div>
+          </div>
         </div>
-        <div className="container-rating"></div>
+        <div className="container-rating">
+          <div className="number">
+            <div className="rating">AVALIAÇÃO EM DESTAQUE</div>
+            <div className="number">
+              <img src={Star} className="star" />
+              10/<div>10</div>
+            </div>
+          </div>
+          <div className="tiny-title">CaioHelbert</div>
+          <div className="incredible">What an incredible sequel!</div>
+          <div className="text">
+            A fantastic and strong continuation of its predecessor, God of War
+            Ragnarök is not afraid to take unexpected turns, or slow its pace
+            for you to fully explore an environment.
+            <br /> It has the same upgrading system as before, but with better
+            armor, in my opinion. Atreus is a little bit older now and it's so
+            great to see everybody reprise their magnificent roles.
+          </div>
+          <div className="text">
+            I don't want to give away too much in this review, but it's
+            definitely worth buying. One of the best looking games on PS5. It'll
+            definitely be a game of the year contender with Elden Ring.
+            <br /> If you're new to the series, I recommend you start with God
+            of War (2018) If you have the dedication, you could always start
+            from the very first one. They're all on the PS+ collection.
+            <br /> I still have yet to beat this newest entry, but I wanted to
+            spread the good word as soon as possible: Ragnarök is here and it is
+            beautiful.
+          </div>
+        </div>
+        <div className="container-rating1">
+          <div className="rating">AVALIAÇÃO EM DESTAQUE</div>
+          <div className="tiny-title">ClaudiaRayara</div>
+          <div className="incredible">This game took part of my soul...</div>
+          <div className="spoiler">Waining: Spoilers!</div>
+        </div>
       </div>
     </>
   );
--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,9 +1,38 @@
+import BigRat from "../../assets/icon/IconRat.svg";
+
 import "./styles.css";
 
 const Footer: React.FC = () => {
   return (
     <>
-      <footer></footer>
+      <footer>
+        <div className="rat">
+          <img src={BigRat} alt="Rato Grande" />
+        </div>
+        <div className="footer-links">
+          <ul>
+            <li className="company">Company</li>
+            <div className="links">About us</div>
+            <div className="links">Partnership</div>
+            <div className="links">About us</div>
+            <div className="links">FAQ</div>
+          </ul>
+          <ul>
+            <li className="company">Company</li>
+            <div className="links">About us</div>
+            <div className="links">Partnership</div>
+            <div className="links">About us</div>
+            <div className="links">FAQ</div>
+          </ul>
+          <ul>
+            <li className="company">Company</li>
+            <div className="links">About us</div>
+            <div className="links">Partnership</div>
+            <div className="links">About us</div>
+            <div className="links">FAQ</div>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 };
